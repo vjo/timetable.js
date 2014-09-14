b7ea9ead1199676da963d64eff35bb58d7d39d3b
Victor Jolissaint <victor.jolissaint@gmail.com>
2014-09-14 17:43:31 +0100

    Add clean target

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     'use strict';
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: ['build/'],
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -44,14 +45,14 @@ module.exports = function(grunt) {
             },
         }
     });
-    //grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
 
     grunt.registerTask('default', [
-        //'clean',
+        'clean',
         'jshint',
         'uglify',
         'sass'
